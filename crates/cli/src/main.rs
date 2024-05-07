c78fe101b113b02b26f39568090c601bf952aab3
Varphone Wong <varphone@qq.com>
2024-05-08 00:09:56 +0800

    Add cargo i18n-export command supports
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -3,6 +3,7 @@ use clap::{Args, Parser};
 use rust_i18n_extract::extractor::Message;
 use rust_i18n_extract::{extractor, generator, iter};
 use rust_i18n_support::{I18nConfig, MinifyKey};
+use std::collections::HashSet;
 use std::{collections::HashMap, path::Path};
 
 #[derive(Parser)]
@@ -10,6 +11,7 @@ use std::{collections::HashMap, path::Path};
 #[command(bin_name = "cargo")]
 enum CargoCli {
     I18n(I18nArgs),
+    I18nExport(I18nExportArgs),
 }
 
 #[derive(Args)]
@@ -37,6 +39,21 @@ struct I18nArgs {
     /// Extract all untranslated I18n texts from source code
     #[arg(default_value = "./", last = true)]
     source: Option<String>,
+}
+
+/// Export I18n to a file with the given format.
+#[derive(Args)]
+#[command(author, version)]
+struct I18nExportArgs {
+    /// The extra locales to export the I18n to.
+    #[arg(short = 'l', long, num_args(0..))]
+    extra_locales: Vec<String>,
+    /// The format to export the I18n to.
+    #[arg(short, long, default_value = "csv")]
+    format: Option<String>,
+    /// The output file to export the I18n to.
+    #[arg(short, long, default_value = "exported")]
+    output: String,
 }
 
 /// Remove quotes from a string at the start and end.
@@ -96,9 +113,7 @@ fn add_translations(
     }
 }
 
-fn main() -> Result<(), Error> {
-    let CargoCli::I18n(args) = CargoCli::parse();
-
+fn i18n(args: I18nArgs) -> Result<(), Error> {
     let mut results = HashMap::new();
 
     let source_path = args.source.expect("Missing source path");
@@ -116,16 +131,72 @@ fn main() -> Result<(), Error> {
     let mut messages: Vec<_> = results.iter().collect();
     messages.sort_by_key(|(_k, m)| m.index);
 
-    let mut has_error = false;
-
     let output_path = Path::new(&source_path).join(&cfg.load_path);
 
-    let result = generator::generate(output_path, &cfg.available_locales, messages.clone());
-    if result.is_err() {
-        has_error = true;
+    generator::generate(output_path, &cfg.available_locales, messages.clone())?;
+
+    Ok(())
+}
+
+fn i18n_export(args: I18nExportArgs) -> Result<(), Error> {
+    let format = args.format.expect("Missing format");
+    let root = std::env::var("CARGO_MANIFEST_DIR").unwrap_or(".".to_string());
+    if let Ok(config) = I18nConfig::load(Path::new(&root)) {
+        let empty_str = String::new();
+        let key = "key".to_string();
+
+        // Load all translations
+        let translations = rust_i18n_support::load_locales(&config.load_path, |_| false);
+
+        // Get all available locales
+        let available_locales: HashSet<_> = config
+            .available_locales
+            .iter()
+            .chain(args.extra_locales.iter())
+            .chain(translations.keys())
+            .collect();
+        let mut sorted_locales: Vec<&String> = available_locales.into_iter().collect();
+        sorted_locales.sort();
+        sorted_locales.insert(0, &key);
+
+        // Get all keys
+        let keys: HashSet<_> = translations
+            .iter()
+            .flat_map(|(_, map)| map.keys())
+            .collect();
+        let mut sorted_keys: Vec<&String> = keys.into_iter().collect();
+        sorted_keys.sort();
+
+        // Write to file
+        let output_file = format!("{}.csv", args.output);
+        let mut csv_writer = csv::WriterBuilder::new()
+            .has_headers(true)
+            .from_path(output_file)?;
+
+        // Write header
+        csv_writer.write_record(&sorted_locales)?;
+
+        // Write rows
+        for key in sorted_keys {
+            let mut row = vec![key];
+            for locale in sorted_locales.iter().skip(1) {
+                let msg: Option<&String> = translations.get(*locale).and_then(|m| m.get(key));
+                row.push(msg.unwrap_or(&empty_str));
+            }
+            csv_writer.write_record(&row)?;
+        }
     }
 
-    if has_error {
+    Ok(())
+}
+
+fn main() -> Result<(), Error> {
+    let result = match CargoCli::parse() {
+        CargoCli::I18n(args) => i18n(args),
+        CargoCli::I18nExport(args) => i18n_export(args),
+    };
+
+    if result.is_err() {
         std::process::exit(1);
     }
 
